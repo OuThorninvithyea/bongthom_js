d407000e03578fba694ca2afbb6e32e6a455bc4f
Chea <Vithyeass@gmail.com>
2026-03-25 15:50:02 +0700

    newchange

--- js/script.js
+++ js/script.js
@@ -121,6 +121,29 @@ const jobQuickListData = [
   { text: "See the Bong Thom dot Com Jobs on Facebook", style: "blue" ,textAlgin: "center" }  
 ]
 
+const otherServicesData = [
+  { text: "Special Schedule", heading: "heading" },
+  { text: "Part-time", count: 19 },
+  { text: "Short-term", count: 6 },
+  { text: "Organization Type", heading: "heading" },
+  { text: "Bar / Restaurant", count: 14 },
+  { text: "Embassy", count: 3 },
+  { text: "Government", count: 3 },
+  { text: "Govt. + NGO Project", count: 2 },
+  { text: "Individual Person", count: 1 },
+  { text: "International Org.", count: 71 },
+  { text: "NGO", count: 80 },
+  { text: "Private Organization", count: 268 },
+  { text: "Real Estate Agent", count: 1 },
+  { text: "School / University", count: 13 },
+  { text: "Other Announcements", heading: "heading"},
+  { text: "Call for Proposals", count: 3 },
+  { text: "Expression of Interest", count: 5 },
+  { text: "Invitation for Bids", count: 4 },
+  { text: "Training / Workshops", count: 4 },
+  { text: "All job categories...", style: "blue" },
+]
+
 
 const joblist = document.querySelector("#jobs-list");
 const dot = `<span class='dot'>●</span>`;
@@ -130,6 +153,7 @@ const newblue = `<img src="./images/new.png" alt="blue">`;
 const classifieds = document.querySelector("#classifieds-list");
 const iconQlQuickList = `<img src="./images/lpicon.png" alt="iconQl">`;
 const jobQuickList = document.querySelector("#job-quick-lists");
+const otherservices = document.querySelector("#other-services");
 
 
 
@@ -145,7 +169,20 @@ function loopJobQuickList() {
   }
 
 }
-loopJobQuickList();
+loopJobQuickList(); 
+
+function loopingOtherservicesData() {
+  otherservices.innerHTML = "";
+
+  for(let i =0; i  < otherServicesData.length; i++){
+    const checkHeading = otherServicesData[i].heading ? "heading" : "";
+    const arrow = otherServicesData[i].heading ? "" : "»";
+    const checkCount = otherServicesData[i].count ? "<span class='count'>(" + otherServicesData[i].count + ") </span>" : "";
+    otherservices.innerHTML += "<tr><td class='sb-pink-row other-services " + checkHeading + "'>" + arrow + " " + otherServicesData[i].text + " " + checkCount + "</td></tr>";
+  }
+}
+
+loopingOtherservicesData();
 
 function loopingjobs() {
   joblist.innerHTML = "";
